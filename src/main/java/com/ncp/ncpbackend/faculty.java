3afcaaf6802b3f4f04dfb508d50dd03caa28fbc7
ragul <ragulcdm@gmail.com>
2021-11-23 01:52:00 +0530

    change2

--- src/main/java/com/ncp/ncpbackend/faculty.java
+++ src/main/java/com/ncp/ncpbackend/faculty.java
@@ -1,8 +1,19 @@
 package com.ncp.ncpbackend;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class faculty {
     @Id
     @GeneratedValue
